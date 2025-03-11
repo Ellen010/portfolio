4d8458a1ebdad6ebf42a78d4a0bfccdd9fd66356
Ellen010 <elenawelty@gmail.com>
2025-03-11 15:14:45 +0100

    hero additions

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -2,44 +2,37 @@ import { FaLocationArrow } from "react-icons/fa6";
 import MagicButton from "./MagicButton"
 import { Spotlight } from "./ui/Spotlight";
 import { TextGenerateEffect } from "./ui/TextGenerateEffect";
+import GridGlobe from "./ui/GridGlobe";
+import { useState, useEffect } from "react";
+import { IoCopyOutline } from "react-icons/io5";
+import dynamic from "next/dynamic";
+import { cn } from "@/lib/utils";
+
 
 const Hero = () => {
   return (
-    <div className="pb-20 pt-36">
-      <div>
-        <Spotlight
-          className="-top-40 -left-10 md:-left-32 md:-top-20 h-screen"
-          fill="white"
-        />
-        <Spotlight
-          className="h-[80vh] w-[50vw] top-10 left-full"
-          fill="purple"
-        />
-        <Spotlight className="left-80 top-28 h-[80vh] w-[50vw]" fill="blue" />
-      </div>
-      <div
-        className="h-screen w-full dark:bg-black-100 bg-white dark:bg-grid-white/[0.03] bg-grid-black-100/[0.2]
-       absolute top-0 left-0 flex items-center justify-center"
-      >
-        <div
-          className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100
-         bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"
+    <div >
+      <div className="w-full absolute left-0 -bottom-72 min-h-96">
+      <img
+          src="/footer-grid.svg"
+          alt="grid"
+          className="w-full h-full opacity-50 "
         />
       </div>
-
       <div className="flex justify-center relative my-20 z-10">
         <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
-          <p className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-80">
+          <p className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-80 my-10">
             Dynamic Web Magic with Next.js
           </p>
 
-          <TextGenerateEffect
+          {/* <TextGenerateEffect
             words="Smart Solutions for Creating a Personalized Online Presence"
-            className="text-center text-[40px] md:text-5xl lg:text-6xl"
-          />
-
+            className="text-center text-[40px] text-white md:text-5xl lg:text-6xl"
+          /> */}
+        
           <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
-            Hi! I&apos;m Elena, a Next.js Developer based in France.
+            Full-stack Next.js Developer based in France
+            <span className="block my-10">Elena Welty</span>
           </p>
 
           <a href="#about">
@@ -49,6 +42,9 @@ const Hero = () => {
               position="right"
             />
           </a>
+          <div className="flex justify-center my-20 z-10">
+           <GridGlobe />
+           </div>
         </div>
       </div>
     </div>
